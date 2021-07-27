0aa68bd7f9e43a95ea5527affb7ecaa5005e7aa2
Maksim Dmitrievich Borisov <burchik017@gmail.com>
2021-07-27 18:52:44 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
+import _ from 'lodash';
 import readlineSync from 'readline-sync';
 
 const getName = (name = 'John') => {
   const gamerName = _.capitalize(readlineSync.question('May I have your name? '));
   if (typeof gamerName !== 'undefined' && gamerName !== '') {
-    gamerName = gamerName[0].toUpperCase() + gamerName.slice(1);
     console.log(`Hello, ${gamerName}!`);
     return gamerName;
   }
