6e5304bbfba65a5f4b18fb224d675cd925d005cb
Maksim Dmitrievich Borisov <burchik017@gmail.com>
2021-07-27 18:48:49 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const getName = (name = 'Gamer') => {
-  let gamerName = readlineSync.question('May I have your name? ');
+const getName = (name = 'John') => {
+  const gamerName = _.capitalize(readlineSync.question('May I have your name? '));
   if (typeof gamerName !== 'undefined' && gamerName !== '') {
     gamerName = gamerName[0].toUpperCase() + gamerName.slice(1);
     console.log(`Hello, ${gamerName}!`);
